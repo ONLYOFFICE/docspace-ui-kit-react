00d3a0c8bb2f45c75791a1f330a2fcc608fc60ae
Timofey <timofey.boyko@onlyoffice.com>
2026-01-23 17:22:22 +0800

    Fix for e2e docspace test

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,49 +3,47 @@ import { useDarkMode } from "storybook-dark-mode";
 
 import { globalColors } from "../themes/globalColors";
 
-import "../styles/custom.scss";
-
 import "./styles.css";
 
 import lightTheme from "./lightTheme";
 import darkTheme from "./darkTheme";
 
 const preview: Preview = {
-	parameters: {
-		backgrounds: { disable: true },
-		controls: {
-			expanded: true,
-			matchers: {
-				color: /(background|color)$/i,
-				date: /Date$/i,
-			},
-		},
-		darkMode: {
-			light: lightTheme,
-			dark: darkTheme,
-		},
-	},
+  parameters: {
+    backgrounds: { disable: true },
+    controls: {
+      expanded: true,
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    darkMode: {
+      light: lightTheme,
+      dark: darkTheme,
+    },
+  },
 
-	decorators: [
-		(Story) => {
-			const isDark = useDarkMode();
+  decorators: [
+    (Story) => {
+      const isDark = useDarkMode();
 
-			return (
-				<div
-					style={{
-						backgroundColor: isDark ? globalColors.black : globalColors.white,
-						color: isDark ? globalColors.white : globalColors.black,
-						minHeight: "100vh",
-						padding: "20px",
-					}}
-				>
-					<Story />
-				</div>
-			);
-		},
-	],
+      return (
+        <div
+          style={{
+            backgroundColor: isDark ? globalColors.black : globalColors.white,
+            color: isDark ? globalColors.white : globalColors.black,
+            minHeight: "100vh",
+            padding: "20px",
+          }}
+        >
+          <Story />
+        </div>
+      );
+    },
+  ],
 
-	tags: ["autodocs"],
+  tags: ["autodocs"],
 };
 
 export default preview;
