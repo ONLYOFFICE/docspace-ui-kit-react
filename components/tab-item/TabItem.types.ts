b353283dbf01d5b57f4cf82a83af2b3df9c55128
Aleksandr Lushkin <aleksandr.lushkin@onlyoffice.com>
2026-01-26 14:51:39 +0100

    TabItem: Add component to ui kit

--- components/tab-item/TabItem.types.ts
+++ components/tab-item/TabItem.types.ts
@@ -24,30 +24,25 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-export * from "./button";
+import React from "react";
 
-export * from "./checkbox";
-
-export * from "./label";
-
-export * from "./portal";
-
-export * from "./tooltip";
-
-export * from "./link";
-
-export * from "./text";
-
-export * from "./text-input";
-
-export * from "./loader";
-
-export * from "./theme-provider";
-
-export * from "./scrollbar";
-
-export * from "./icon-button";
-
-export * from "./toggle-button";
-
-export * from "./tab-item";
+export type TTabItemProps = {
+  /** Tab item text. */
+  label: string | React.ReactNode;
+  /** Callback function when tab is selected. */
+  onSelect?: (event: React.MouseEvent<HTMLDivElement>) => void;
+  /** Determines if the tab is currently active. */
+  isActive?: boolean;
+  /** Determines if the tab is disabled and cannot be interacted with. */
+  isDisabled?: boolean;
+  /** Allows the tab to be deselected, resulting in no active tab. */
+  allowNoSelection?: boolean;
+  /** Enables multi-select functionality */
+  withMultiSelect?: boolean;
+  /** Sets a tab class name */
+  className?: string;
+  /** Sets a tab data-testid */
+  dataTestId?: string;
+  /** Locks the last selected tab */
+  lockLastSelection?: boolean;
+};
