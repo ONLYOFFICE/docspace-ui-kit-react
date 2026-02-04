c3804da07e43110b710afbfd0f07bc5d29d9b610
DmitrySychugov <dmitry.sychugov@onlyoffice.com>
2026-02-04 17:05:49 +0500

    FloatingButton: move to ui-kit

--- components/floating-button/FloatingButton.enums.ts
+++ components/floating-button/FloatingButton.enums.ts
@@ -24,44 +24,23 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-import { globalColors } from "../themes";
+import { OPERATIONS_NAME } from "../../constants";
 
-export const LOADER_STYLE = Object.freeze({
-  title: "",
-  width: "100%",
-  height: "32",
-  backgroundColor: globalColors.darkBlack,
-  foregroundColor: globalColors.darkBlack,
-  backgroundOpacity: 0.1,
-  foregroundOpacity: 0.15,
-  borderRadius: "3",
-  radius: "3",
-  speed: 2,
-  animate: true,
-});
-
-export const OPERATIONS_NAME = Object.freeze({
-  trash: "trash",
-  deletePermanently: "deletePermanently",
-  download: "download",
-  duplicate: "duplicate",
-  exportIndex: "exportIndex",
-  markAsRead: "markAsRead",
-  copy: "copy",
-  move: "move",
-  convert: "convert",
-  other: "other",
-  upload: "upload",
-  deleteVersionFile: "deleteVersionFile",
-  backup: "backup",
-});
-
-export const ROOM_ACTION_KEYS = {
-  CREATE_EDIT_ROOM_UPLOAD: "create_edit_room_upload",
-  CREATE_EDIT_ROOM_DELETE: "create_edit_room_delete",
-  CREATE_EDIT_ROOM_CUSTOMIZE_COVER: "create_edit_room_customize_cover",
+export const FloatingButtonIcons = {
+  upload: OPERATIONS_NAME.upload,
+  trash: OPERATIONS_NAME.trash,
+  move: OPERATIONS_NAME.move,
+  duplicate: OPERATIONS_NAME.duplicate,
+  plus: "plus",
+  minus: "minus",
+  refresh: "refresh",
+  exportIndex: OPERATIONS_NAME.exportIndex,
+  dots: "dots",
+  arrow: "arrow",
+  deletePermanently: OPERATIONS_NAME.deletePermanently,
+  download: OPERATIONS_NAME.download,
+  copy: OPERATIONS_NAME.copy,
+  markAsRead: OPERATIONS_NAME.markAsRead,
+  other: OPERATIONS_NAME.other,
+  backup: OPERATIONS_NAME.backup,
 } as const;
-
-export const ASIDE_PADDING_AFTER_LAST_ITEM = "12px";
-
-export const LIVE_CHAT_LOCAL_STORAGE_KEY = "live_chat_state";
