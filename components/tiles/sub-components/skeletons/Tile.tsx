143b301e4a57fd86e987a9123aee05852ddf312d
DmitrySychugov <dmitry.sychugov@onlyoffice.com>
2026-02-06 18:39:23 +0500

    Tiles: Skeletons: move to ui-kit

---
+++ components/tiles/sub-components/skeletons/Tile.tsx
@@ -0,0 +1,159 @@
+// (c) Copyright Ascensio System SIA 2009-2026
+//
+// This program is a free software product.
+// You can redistribute it and/or modify it under the terms
+// of the GNU Affero General Public License (AGPL) version 3 as published by the Free Software
+// Foundation. In accordance with Section 7(a) of the GNU AGPL its Section 15 shall be amended
+// to the effect that Ascensio System SIA expressly excludes the warranty of non-infringement of
+// any third-party rights.
+//
+// This program is distributed WITHOUT ANY WARRANTY, without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For details, see
+// the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
+//
+// You can contact Ascensio System SIA at Lubanas st. 125a-25, Riga, Latvia, EU, LV-1021.
+//
+// The  interactive user interfaces in modified source and object code versions of the Program must
+// display Appropriate Legal Notices, as required under Section 5 of the GNU AGPL version 3.
+//
+// Pursuant to Section 7(b) of the License you must retain the original Product logo when
+// distributing the program. Pursuant to Section 7(e) we decline to grant you any rights under
+// trademark law for use of our trademarks.
+//
+// All the Product's GUI elements, including illustrations and icon sets, as well as technical writing
+// content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
+// International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
+
+import React from "react";
+
+import classNames from "classnames";
+
+import { RectangleSkeleton } from "../../../rectangle";
+import styles from "./Tiles.module.scss";
+
+import type { TileSkeletonProps } from "./Tiles.types";
+
+export const TileSkeleton = ({
+  isFolder,
+  isRoom,
+  title,
+  borderRadius,
+  backgroundColor,
+  foregroundColor,
+  backgroundOpacity,
+  foregroundOpacity,
+  speed,
+  className,
+  ...rest
+}: TileSkeletonProps) => {
+  return isFolder ? (
+    <div
+      className={classNames(styles.tile, "bottom-content", className)}
+      data-testid="tile-skeleton-folder"
+      {...rest}
+    >
+      <RectangleSkeleton
+        title={title}
+        width="100%"
+        height="64px"
+        borderRadius={borderRadius || "12px"}
+        backgroundColor={backgroundColor}
+        foregroundColor={foregroundColor}
+        backgroundOpacity={backgroundOpacity}
+        foregroundOpacity={foregroundOpacity}
+        speed={speed}
+        animate
+      />
+    </div>
+  ) : isRoom ? (
+    <div
+      className={classNames(styles.tile, className)}
+      data-testid="tile-skeleton-room"
+      {...rest}
+    >
+      <div className={styles.roomTile} data-testid="room-tile-content">
+        <div className={styles.roomTileTopContent}>
+          <RectangleSkeleton
+            title={title}
+            width="32px"
+            height="32px"
+            borderRadius={borderRadius}
+            backgroundColor={backgroundColor}
+            foregroundColor={foregroundColor}
+            backgroundOpacity={backgroundOpacity}
+            foregroundOpacity={foregroundOpacity}
+            speed={speed}
+            animate
+          />
+          <RectangleSkeleton
+            title={title}
+            height="22px"
+            borderRadius={borderRadius}
+            backgroundColor={backgroundColor}
+            foregroundColor={foregroundColor}
+            backgroundOpacity={backgroundOpacity}
+            foregroundOpacity={foregroundOpacity}
+            speed={speed}
+            animate
+          />
+          <RectangleSkeleton
+            title={title}
+            height="16px"
+            width="16px"
+            borderRadius={borderRadius}
+            backgroundColor={backgroundColor}
+            foregroundColor={foregroundColor}
+            backgroundOpacity={backgroundOpacity}
+            foregroundOpacity={foregroundOpacity}
+            speed={speed}
+            animate
+          />
+        </div>
+        <div className={styles.roomTileBottomContent}>
+          <RectangleSkeleton
+            title={title}
+            height="24px"
+            width="50px"
+            borderRadius={borderRadius}
+            backgroundColor={backgroundColor}
+            foregroundColor={foregroundColor}
+            backgroundOpacity={backgroundOpacity}
+            foregroundOpacity={foregroundOpacity}
+            speed={speed}
+            animate
+          />
+          <RectangleSkeleton
+            title={title}
+            height="24px"
+            width="50px"
+            borderRadius={borderRadius}
+            backgroundColor={backgroundColor}
+            foregroundColor={foregroundColor}
+            backgroundOpacity={backgroundOpacity}
+            foregroundOpacity={foregroundOpacity}
+            speed={speed}
+            animate
+          />
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div
+      className={classNames(styles.tile, styles.file, className)}
+      data-testid="tile-skeleton-file"
+      {...rest}
+    >
+      <RectangleSkeleton
+        title={title}
+        height="220px"
+        borderRadius={borderRadius || "12px"}
+        backgroundColor={backgroundColor}
+        foregroundColor={foregroundColor}
+        backgroundOpacity={backgroundOpacity}
+        foregroundOpacity={foregroundOpacity}
+        speed={speed}
+        animate
+      />
+    </div>
+  );
+};
