cf43b8b7d6d1e2ccaa5db1c25d98593ecebebb8d
Timofey <timofey.boyko@onlyoffice.com>
2026-02-09 20:30:49 +0800

    Update test setup

--- test/setup.ts
+++ test/setup.ts
@@ -1,1 +1,11 @@
 import "@testing-library/jest-dom/vitest";
+
+import enCommon from "../locales/en/Common.json";
+import type { TTranslations } from "../providers/translation/i18n";
+import { getI18NInstance } from "../providers/translation/i18n";
+
+const translations: TTranslations = new Map([
+  ["en", new Map([["Common", enCommon]])],
+]);
+
+getI18NInstance("en", translations);
