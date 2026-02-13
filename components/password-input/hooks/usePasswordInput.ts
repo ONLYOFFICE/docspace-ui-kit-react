6a8e3fd5277a059daa912d3076fd070a11a55dde
Timofey <timofey.boyko@onlyoffice.com>
2026-02-13 16:55:22 +0800

    Update after merge 'develop'

--- components/password-input/hooks/usePasswordInput.ts
+++ components/password-input/hooks/usePasswordInput.ts
@@ -30,109 +30,115 @@ import { InputType } from "../../text-input";
 import { TPasswordState } from "../PasswordInput.types";
 
 export const usePasswordInput = (
-  isSimulateType: boolean,
-  simulateSymbol: string,
-  simpleView: boolean,
-  type: InputType.text | InputType.password,
-  checkPassword: (
-    value: string,
-    setState: React.Dispatch<React.SetStateAction<TPasswordState>>,
-  ) => void,
-  setState: React.Dispatch<React.SetStateAction<TPasswordState>>,
-  onChange?: (e: ChangeEvent<HTMLInputElement>, value?: string) => void,
-  valueInput?: string,
+	isSimulateType: boolean,
+	simulateSymbol: string,
+	simpleView: boolean,
+	type: InputType.text | InputType.password,
+	checkPassword: (
+		value: string,
+		setState: React.Dispatch<React.SetStateAction<TPasswordState>>,
+	) => void,
+	setState: React.Dispatch<React.SetStateAction<TPasswordState>>,
+	onChange?: (e: ChangeEvent<HTMLInputElement>, value?: string) => void,
+	valueInput?: string,
+	sanitizeValue?: (value: string) => string,
 ) => {
-  const [caretPosition, setCaretPosition] = useState<number | null>(null);
+	const [caretPosition, setCaretPosition] = useState<number | null>(null);
 
-  const setPasswordSettings = useCallback(
-    (newPassword: string) => {
-      let newValue;
+	const setPasswordSettings = useCallback(
+		(newPassword: string) => {
+			let newValue;
 
-      if (!valueInput) return newPassword;
+			if (!valueInput) return newPassword;
 
-      const oldPassword = valueInput ?? "";
-      const oldPasswordLength = oldPassword.length;
-      const newCaretPosition = (
-        document.getElementById("conversion-password") as HTMLInputElement
-      )?.selectionStart;
+			const oldPassword = valueInput ?? "";
+			const oldPasswordLength = oldPassword.length;
+			const newCaretPosition = (
+				document.getElementById("conversion-password") as HTMLInputElement
+			)?.selectionStart;
 
-      setCaretPosition(newCaretPosition);
-      const newCharactersUntilCaret = newPassword.substring(
-        0,
-        newCaretPosition ?? undefined,
-      );
+			setCaretPosition(newCaretPosition);
+			const newCharactersUntilCaret = newPassword.substring(
+				0,
+				newCaretPosition ?? undefined,
+			);
 
-      const unchangedStartCharacters = newCharactersUntilCaret
-        .split("")
-        .filter((el) => el === simulateSymbol).length;
+			const unchangedStartCharacters = newCharactersUntilCaret
+				.split("")
+				.filter((el) => el === simulateSymbol).length;
 
-      const unchangedEndingCharacters = newCaretPosition
-        ? newPassword.substring(newCaretPosition).length
-        : 0;
-      const addedCharacters = newCharactersUntilCaret.substring(
-        unchangedStartCharacters,
-      );
+			const unchangedEndingCharacters = newCaretPosition
+				? newPassword.substring(newCaretPosition).length
+				: 0;
+			const addedCharacters = newCharactersUntilCaret.substring(
+				unchangedStartCharacters,
+			);
 
-      const startingPartOldPassword = oldPassword.substring(
-        0,
-        unchangedStartCharacters,
-      );
-      const countOfCharacters = oldPasswordLength - unchangedEndingCharacters;
-      const endingPartOldPassword = oldPassword.substring(countOfCharacters);
+			const startingPartOldPassword = oldPassword.substring(
+				0,
+				unchangedStartCharacters,
+			);
+			const countOfCharacters = oldPasswordLength - unchangedEndingCharacters;
+			const endingPartOldPassword = oldPassword.substring(countOfCharacters);
 
-      newValue = startingPartOldPassword + addedCharacters;
+			newValue = startingPartOldPassword + addedCharacters;
 
-      if (unchangedEndingCharacters) {
-        newValue += endingPartOldPassword;
-      }
+			if (unchangedEndingCharacters) {
+				newValue += endingPartOldPassword;
+			}
 
-      return newValue;
-    },
-    [simulateSymbol, valueInput],
-  );
+			return newValue;
+		},
+		[simulateSymbol, valueInput],
+	);
 
-  const onChangeAction = useCallback(
-    (e: ChangeEvent<HTMLInputElement>, isGenerated?: boolean) => {
-      let { value } = e.target;
-      if (isSimulateType && !isGenerated) {
-        value = setPasswordSettings(e.target.value);
-      }
+	const onChangeAction = useCallback(
+		(e: ChangeEvent<HTMLInputElement>, isGenerated?: boolean) => {
+			let { value } = e.target;
+			if (isSimulateType && !isGenerated) {
+				value = setPasswordSettings(e.target.value);
+			}
 
-      onChange?.(e, value);
+			if (sanitizeValue) {
+				value = sanitizeValue(value);
+			}
 
-      if (simpleView) {
-        setState((s) => ({
-          ...s,
-          value,
-        }));
-        return;
-      }
+			onChange?.(e, value);
 
-      checkPassword(value, setState);
-    },
-    [
-      isSimulateType,
-      onChange,
-      simpleView,
-      checkPassword,
-      setState,
-      setPasswordSettings,
-    ],
-  );
+			if (simpleView) {
+				setState((s) => ({
+					...s,
+					value,
+				}));
+				return;
+			}
 
-  useEffect(() => {
-    if (caretPosition && isSimulateType && type === InputType.password) {
-      const input = document.getElementById(
-        "conversion-password",
-      ) as HTMLInputElement;
-      input?.setSelectionRange(caretPosition, caretPosition);
-    }
-  }, [caretPosition, isSimulateType, type]);
+			checkPassword(value, setState);
+		},
+		[
+			isSimulateType,
+			onChange,
+			simpleView,
+			checkPassword,
+			setState,
+			setPasswordSettings,
+			sanitizeValue,
+		],
+	);
 
-  return {
-    caretPosition,
-    setCaretPosition,
-    setPasswordSettings,
-    onChangeAction,
-  };
+	useEffect(() => {
+		if (caretPosition && isSimulateType && type === InputType.password) {
+			const input = document.getElementById(
+				"conversion-password",
+			) as HTMLInputElement;
+			input?.setSelectionRange(caretPosition, caretPosition);
+		}
+	}, [caretPosition, isSimulateType, type]);
+
+	return {
+		caretPosition,
+		setCaretPosition,
+		setPasswordSettings,
+		onChangeAction,
+	};
 };
