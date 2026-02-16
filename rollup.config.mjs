db264d11872469b67f9ddd20fbfedbc5de4bc621
Alexander Evdokimov <aleksandr.evdokimov@onlyoffice.com>
2026-02-16 10:48:41 +0300

    ui-kit: build: add minifaction js code

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,10 +3,10 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import svgr from "@svgr/rollup";
-import dts from "rollup-plugin-dts";
-import nodePolyfills from "rollup-plugin-polyfill-node";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
+import terser from "@rollup/plugin-terser";
+import nodePolyfills from "rollup-plugin-polyfill-node";
 import { glob } from "glob";
 
 // get all index.ts files from subfolders
@@ -80,6 +80,7 @@ export default [
 				declaration: false,
 				declarationDir: undefined,
 			}),
+			terser(),
 			postcss({
 				modules: {
 					generateScopedName: "[hash:base64:5]",
