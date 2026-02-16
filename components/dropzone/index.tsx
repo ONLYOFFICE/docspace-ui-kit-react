5cd2673d87e77bc724ce05fdf9b463243c2b8968
Darya Umrikhina <darya.umrihina@onlyoffice.com>
2026-02-16 18:02:04 +0400

    Components:Dropzone: update with new functionality for SDK

--- components/dropzone/index.tsx
+++ components/dropzone/index.tsx
@@ -25,14 +25,19 @@
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
 import React from "react";
-import { useDropzone } from "react-dropzone";
+import { useDropzone, DropEvent } from "react-dropzone";
 import classNames from "classnames";
 
+import TriangleDownIcon from "PUBLIC_DIR/images/triangle.down.react.svg";
+
 import { Loader, LoaderTypes } from "../loader";
-import { Link } from "../link";
+import { Badge } from "../badge";
+import { DropDown } from "../drop-down";
+import { IconSizeType } from "../../utils";
 
 import { DropzoneProps } from "./Dropzone.types";
 import styles from "./Dropzone.module.scss";
+import { Link } from "../link";
 
 const Dropzone = ({
   isLoading,
@@ -40,22 +45,82 @@ const Dropzone = ({
   onDrop,
   accept,
   maxFiles = 0,
-  linkMainText,
+  getFilesFromEvent,
+  linkMainTextForFiles,
+  linkMainTextForFolders,
   linkSecondaryText,
   exstsText,
+  fullExstsText,
+  formatsPlusBadgeValue,
   dataTestId,
+  icon,
+  iconClassName,
+  className,
+  loaderClassName,
 }: DropzoneProps) => {
-  const { getRootProps, getInputProps } = useDropzone({
+  const folderInputRef = React.useRef<HTMLInputElement>(null);
+
+  const dropzoneOptions = {
     maxFiles,
-    noClick: isDisabled,
+    noClick: isDisabled || !!linkMainTextForFolders,
     noKeyboard: isDisabled,
-    accept,
+    noDrag: isDisabled,
+    ...(accept ? { accept } : {}),
     onDrop,
-  });
+    ...(getFilesFromEvent
+      ? {
+          getFilesFromEvent: (event: DropEvent) =>
+            Promise.resolve(getFilesFromEvent(event)),
+        }
+      : {}),
+  } as Parameters<typeof useDropzone>[0];
+
+  const { getRootProps, getInputProps, open } = useDropzone(dropzoneOptions);
+
+  const handleFileClick = (e: React.MouseEvent) => {
+    if (linkMainTextForFolders && !isDisabled) {
+      e.stopPropagation();
+      e.preventDefault();
+      open();
+    }
+  };
+
+  const handleFolderClick = (e: React.MouseEvent) => {
+    if (linkMainTextForFolders && !isDisabled) {
+      e.stopPropagation();
+      e.preventDefault();
+      folderInputRef.current?.click();
+    }
+  };
+
+  const handleFolderChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files || !getFilesFromEvent) return;
+    const files = await getFilesFromEvent(e as unknown as DropEvent);
+    if (onDrop) {
+      onDrop(files as File[]);
+    }
+  };
+
+  const [isFormatsOpen, setIsFormatsOpen] = React.useState(false);
+  const formatsRef = React.useRef<HTMLDivElement>(null);
+
+  const handleFormatsClick = (e: React.MouseEvent) => {
+    if (fullExstsText) {
+      e.stopPropagation();
+      e.preventDefault();
+      setIsFormatsOpen((prev) => !prev);
+    }
+  };
+
+  const handleFormatsClose = (e: Event, open: boolean) => {
+    if (!open) {
+      setIsFormatsOpen(false);
+    }
+  };
 
   return (
     <div
-      className={classNames(styles.dropzoneWrapper, {
+      className={classNames(styles.dropzoneWrapper, className, {
         [styles.isLoading]: isLoading,
       })}
       data-testid={dataTestId ?? "dropzone"}
@@ -64,14 +129,14 @@ const Dropzone = ({
     >
       {isLoading ? (
         <Loader
-          className={styles.dropzoneLoader}
+          className={classNames(styles.dropzoneLoader, loaderClassName)}
           size="30px"
           type={LoaderTypes.track}
         />
       ) : (
         <div
           {...getRootProps({
-            className: styles.dropzone,
+            className: classNames(styles.dropzone),
             "aria-label": "File upload area",
             "data-testid": "dropzone-input-area",
           })}
@@ -83,6 +148,29 @@ const Dropzone = ({
               "data-testid": "dropzone-input",
             })}
           />
+          {!!linkMainTextForFolders && (
+            <input
+              ref={folderInputRef}
+              type="file"
+              disabled={isDisabled}
+              style={{ display: "none" }}
+              onChange={handleFolderChange}
+              {...({
+                webkitdirectory: "",
+                directory: "",
+              } as React.InputHTMLAttributes<HTMLInputElement>)}
+              aria-label="Folder input"
+              data-testid="dropzone-folder-input"
+            />
+          )}
+          {icon && (
+            <img
+              src={icon}
+              alt="Upload"
+              className={classNames(styles.dropzoneIcon, iconClassName)}
+              data-testid="dropzone-icon"
+            />
+          )}
           <div
             className={styles.dropzoneLink}
             data-testid="dropzone-text"
@@ -95,9 +183,31 @@ const Dropzone = ({
               })}
               data-testid="dropzone-main-text"
               color="accent"
+              onClick={linkMainTextForFolders ? handleFileClick : undefined}
             >
-              {linkMainText}
+              {linkMainTextForFiles}
             </Link>
+            {!!linkMainTextForFolders && (
+              <>
+                <span
+                  className={classNames(styles.dropzoneLink, {
+                    [styles.secondary]: true,
+                  })}
+                >
+                  {" or "}
+                </span>
+                <Link
+                  className={classNames(styles.dropzoneLink, {
+                    [styles.main]: true,
+                  })}
+                  data-testid="dropzone-folder-text"
+                  color="accent"
+                  onClick={handleFolderClick}
+                >
+                  {linkMainTextForFolders}
+                </Link>
+              </>
+            )}
             <span
               className={classNames(styles.dropzoneLink, {
                 [styles.secondary]: true,
@@ -108,11 +218,54 @@ const Dropzone = ({
             </span>
           </div>
           <div
-            className={styles.dropzoneExsts}
+            ref={formatsRef}
+            className={classNames(styles.dropzoneExsts, {
+              [styles.clickable]: !!fullExstsText,
+            })}
             data-testid="dropzone-file-types"
             aria-label="Supported file types"
+            onClick={handleFormatsClick}
           >
-            {exstsText}
+            <div
+              className={classNames(styles.dropzoneExstsTextContainer, {
+                [styles.isOpen]: isFormatsOpen,
+                [styles.clickable]: !!fullExstsText,
+              })}
+            >
+              <span className={styles.dropzoneExstsText}>{exstsText}</span>
+              {formatsPlusBadgeValue && formatsPlusBadgeValue > 0 ? (
+                <Badge
+                  className={styles.dropzoneExstsBadge}
+                  label={`+${formatsPlusBadgeValue}`}
+                  isMutedBadge
+                  borderRadius="50px"
+                />
+              ) : null}
+              {fullExstsText ? (
+                <TriangleDownIcon
+                  data-size={IconSizeType.scale}
+                  className={classNames(styles.dropzoneArrowIcon, {
+                    [styles.isOpen]: isFormatsOpen,
+                  })}
+                />
+              ) : null}
+              {fullExstsText ? (
+                <DropDown
+                  className={styles.dropzoneFormatsDropdown}
+                  open={isFormatsOpen}
+                  clickOutsideAction={handleFormatsClose}
+                  forwardedRef={formatsRef}
+                  manualY="4"
+                  directionY="bottom"
+                  withBackdrop={false}
+                  isDefaultMode={false}
+                >
+                  <div className={styles.dropzoneFormatsContent}>
+                    {fullExstsText}
+                  </div>
+                </DropDown>
+              ) : null}
+            </div>
           </div>
         </div>
       )}
