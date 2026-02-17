c96720ca0584f9914c459740acc2927203febe60
Alexander Evdokimov <aleksandr.evdokimov@onlyoffice.com>
2026-02-17 10:18:21 +0300

    ui-kit: rollup: add custom plugin to keep only .d.ts files

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -10,111 +10,134 @@ import nodePolyfills from "rollup-plugin-polyfill-node";
 import { glob } from "glob";
 
 // get all index.ts files from subfolders
-const ignorePatterns = ["**/*.test.{ts,tsx}", "**/*.spec.{ts,tsx}", "**/*.stories.{ts,tsx}"];
+const ignorePatterns = [
+  "**/*.test.{ts,tsx}",
+  "**/*.spec.{ts,tsx}",
+  "**/*.stories.{ts,tsx}",
+];
 
-const componentEntries = glob.sync("components/*/index.ts", { ignore: ignorePatterns });
+const componentEntries = glob.sync("components/*/index.ts", {
+  ignore: ignorePatterns,
+});
 const hookEntries = glob.sync("hooks/*/index.ts", { ignore: ignorePatterns });
-const providerEntries = glob.sync("providers/*/index.ts", { ignore: ignorePatterns });
+const providerEntries = glob.sync("providers/*/index.ts", {
+  ignore: ignorePatterns,
+});
 const utilEntries = glob.sync("utils/*/index.ts", { ignore: ignorePatterns });
-const contextEntries = glob.sync("context/*/index.ts", { ignore: ignorePatterns });
+const contextEntries = glob.sync("context/*/index.ts", {
+  ignore: ignorePatterns,
+});
 const enumEntries = glob.sync("enums/*/index.ts", { ignore: ignorePatterns });
-const constantEntries = glob.sync("constants/*/index.ts", { ignore: ignorePatterns });
+const constantEntries = glob.sync("constants/*/index.ts", {
+  ignore: ignorePatterns,
+});
 const typeEntries = glob.sync("types/*/index.ts", { ignore: ignorePatterns });
 const errorEntries = glob.sync("errors/*/index.ts", { ignore: ignorePatterns });
 
 // merge all entry points
 const allEntries = [
-	"index.ts",
-	"components/index.ts",
-	"hooks/index.ts",
-	"providers/index.ts",
-	"utils/index.ts",
-	"context/index.ts",
-	"enums/index.ts",
-	"constants/index.ts",
-	"types/index.ts",
-	"errors/index.ts",
-	...componentEntries,
-	...hookEntries,
-	...providerEntries,
-	...utilEntries,
-	...contextEntries,
-	...enumEntries,
-	...constantEntries,
-	...typeEntries,
-	...errorEntries,
+  "index.ts",
+  "components/index.ts",
+  "hooks/index.ts",
+  "providers/index.ts",
+  "utils/index.ts",
+  "context/index.ts",
+  "enums/index.ts",
+  "constants/index.ts",
+  "types/index.ts",
+  "errors/index.ts",
+  ...componentEntries,
+  ...hookEntries,
+  ...providerEntries,
+  ...utilEntries,
+  ...contextEntries,
+  ...enumEntries,
+  ...constantEntries,
+  ...typeEntries,
+  ...errorEntries,
 ];
 
 export default [
-	{
-		input: allEntries,
-		output: [
-			{
-				dir: "dist/esm",
-				format: "esm",
-				sourcemap: false,
-				preserveModules: true,
-				preserveModulesRoot: ".",
-			},
-			{
-				dir: "dist/cjs",
-				format: "cjs",
-				sourcemap: false,
-				preserveModules: true,
-				preserveModulesRoot: ".",
-			},
-		],
-		plugins: [
-			peerDepsExternal(),
-			resolve({
-				extensions: [".ts", ".tsx", ".js", ".jsx"],
-				browser: true,
-				preferBuiltins: false,
-			}),
-			nodePolyfills(),
-			svgr(),
-			json(),
-			commonjs(),
-			typescript({
-				tsconfig: "./tsconfig.build.json",
-				declaration: false,
-				declarationDir: undefined,
-			}),
-			terser(),
-			postcss({
-				modules: {
-					generateScopedName: "[hash:base64:5]",
-				},
-				inject: true,
-				minimize: true,
-				use: [
-				[
-					"sass",
-					{
-						silenceDeprecations: ["legacy-js-api"],
-					},
-				],
-			],
-			}),
-		],
-		external: ["react", "react-dom", "react/jsx-runtime"],
-	},
-	{
-		input: allEntries,
-		output: {
-			dir: 'dist/types',
-			format: "esm",
-			preserveModules: true,
-			preserveModulesRoot: '.',
-		},
-		plugins: [
-			typescript({
-				tsconfig: './tsconfig.build.json',
-				declaration: true,
-				declarationDir: 'dist/types',
-				emitDeclarationOnly: true,
-			  }),
-		],
-		external: [/\.scss$/, /\.css$/, /\.svg$/],
-	},
+  {
+    input: allEntries,
+    output: [
+      {
+        dir: "dist/esm",
+        format: "esm",
+        sourcemap: false,
+        preserveModules: true,
+        preserveModulesRoot: ".",
+      },
+      {
+        dir: "dist/cjs",
+        format: "cjs",
+        sourcemap: false,
+        preserveModules: true,
+        preserveModulesRoot: ".",
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve({
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+        browser: true,
+        preferBuiltins: false,
+      }),
+      nodePolyfills(),
+      svgr(),
+      json(),
+      commonjs(),
+      typescript({
+        tsconfig: "./tsconfig.build.json",
+        declaration: false,
+        declarationDir: undefined,
+      }),
+      terser(),
+      postcss({
+        modules: {
+          generateScopedName: "[hash:base64:5]",
+        },
+        inject: true,
+        minimize: true,
+        use: [
+          [
+            "sass",
+            {
+              silenceDeprecations: ["legacy-js-api"],
+            },
+          ],
+        ],
+      }),
+    ],
+    external: ["react", "react-dom", "react/jsx-runtime"],
+  },
+  {
+    input: allEntries,
+    output: {
+      dir: "dist/types",
+      preserveModules: true,
+      preserveModulesRoot: ".",
+    },
+    plugins: [
+      typescript({
+        tsconfig: "./tsconfig.build.json",
+        declaration: true,
+        declarationDir: "dist/types",
+        emitDeclarationOnly: true,
+        outDir: "dist/types",
+      }),
+      // Custom plugin to keep only .d.ts files
+      {
+        name: "keep-only-dts-files",
+        generateBundle(options, bundle) {
+          for (const fileName in bundle) {
+            if (!fileName.endsWith(".d.ts")) {
+              delete bundle[fileName];
+            }
+          }
+        },
+      },
+    ],
+    external: [/\.scss$/, /\.css$/, /\.svg$/, /node_modules/],
+  },
 ];
