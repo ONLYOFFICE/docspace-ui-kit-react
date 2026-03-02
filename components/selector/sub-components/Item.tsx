006e245e148c9daf0e0e182e2e45631ba2ed6156
Timofey <timofey.boyko@onlyoffice.com>
2026-03-02 16:21:11 +0800

    Fix tsc

--- components/selector/sub-components/Item.tsx
+++ components/selector/sub-components/Item.tsx
@@ -292,6 +292,8 @@ const Item = React.memo(({ index, style, data }: ItemProps) => {
               badgeIconNode={badgeIconNode ?? undefined}
               isTemplate={isTemplate}
             />
+          ) : React.isValidElement(icon) ? (
+            <div className={styles.itemLogo}>{icon}</div>
           ) : (
             <div className={styles.itemLogo}>{React.createElement(icon)}</div>
           )
