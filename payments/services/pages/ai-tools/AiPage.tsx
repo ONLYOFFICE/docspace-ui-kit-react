451c8d49c0a15da61aae5460d49820325e2b9e7f
TatianaLopaeva <tatiana.lopaeva@onlyoffice.com>
2026-04-07 17:34:37 +0300

    Fixed ts error.

--- payments/services/pages/ai-tools/AiPage.tsx
+++ payments/services/pages/ai-tools/AiPage.tsx
@@ -107,7 +107,7 @@ const AiPage = (props: AiPageProps) => {
   const [isConfirmDialogVisible, setIsConfirmDialogVisible] = useState(false);
   const [isTopUpConfirmVisible, setIsTopUpConfirmVisible] = useState(false);
 
-  const isDisabled = isServiceActionDisabled;
+  const isDisabled = isServiceActionDisabled!;
 
   const navigate = useNavigate();
 
