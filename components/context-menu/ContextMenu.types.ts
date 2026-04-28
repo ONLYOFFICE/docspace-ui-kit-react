db81a10d2c599ba9d00c5ee56e4cd724b0b44076
gopienkonikita <nikita.gopienko@onlyoffice.com>
2026-04-28 12:27:52 +0300

    ContextMenu, RoomIcon: added badgeIconColor

--- components/context-menu/ContextMenu.types.ts
+++ components/context-menu/ContextMenu.types.ts
@@ -120,6 +120,7 @@ export type HeaderType =
       logo?: string;
       icon?: string;
       badgeUrl?: string;
+      badgeIconColor?: string;
     })
   | { title: string; icon: string; badgeUrl?: string };
 
@@ -193,6 +194,7 @@ export interface ContextMenuProps {
   isArchive?: boolean;
   ref?: React.RefObject<ContextMenuRefType | null>;
   badgeUrl?: string;
+  badgeIconColor?: string;
   headerOnlyMobile?: boolean;
   dataTestId?: string;
   maxHeightLowerSubmenu?: number;
